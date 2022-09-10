a164fd6e786f97749159d592f096b3f2c0e2ced9
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2022-09-10 22:31:13 +0530

    Time: 130 ms (32.84%), Space: 25.1 MB (54.27%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
     int minimumJumps(vector<int>& forb, int a, int b, int x) {
-        map<pair<int,int>,int>vis1;
+        if(x==0)
+            return 0;
+        unordered_map<int,int>vis1;
+        unordered_map<int,int>vis2;
         for(int i=0;i<forb.size();i++)
         {
-            vis1[{forb[i],0}]=1;
-            vis1[{forb[i],1}]=1;
+            vis1[forb[i]]=1;
+            vis2[forb[i]]=1;
         }
         queue<pair<int,int>>q;
         q.push({0,1});
-        int count=0;
+        vis1[0]=1;
+        vis2[0]=1;
+        int count=1;
         while(!q.empty())
         {
             int s=q.size();
             while(s--)
             {
                 auto i=q.front();
-                q.pop();
-                if(i.first==x)
-                    return count;
-                if(vis1[i])
-                    continue;
-                vis1[i]=1;
                 int dir1=i.first+a,dir2=i.first-b;
-                if(dir1<=max(x+a+b,5000)){
+                if(vis1[dir1]==0 && dir1<=max(x+a+b,5000)){
+                    // cout<<dir1<<" ";
+                    if(dir1==x)
+                        return count;
                     q.push({dir1,0});
+                    vis1[dir1]=1;
                 }
-                if(i.second==0 && dir2>0){
+                if(i.second==0 && dir2>0 && vis2[dir2]==0){
+                    // cout<<dir2<<" ";
+                    if(dir2==x)
+                        return count;
                     q.push({dir2,1});
-                } 
+                    vis2[dir2]=1;
+                }
+                q.pop();
+                
             }
             count++;
         }
