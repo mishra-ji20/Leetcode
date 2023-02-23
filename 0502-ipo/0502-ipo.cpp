8ef9c7383a09b643597d483eac5e2b819e04f0f0
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-02-23 23:32:30 +0530

    Time: 264 ms (41.65%), Space: 82.1 MB (23.74%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        set<pair<int,int>>st;
-        vector<pair<int,int>>arr;
-        int a=0;
+       priority_queue<int>bucket;
+        priority_queue<pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>>q; 
         for(int i=0;i<profits.size();i++)
         {
-            arr.push_back({capital[i],profits[i]});
+            q.push({capital[i],profits[i]});
         }
-        sort(arr.begin(),arr.end());
-        int sum=w;
-        for(int i=0;i<arr.size();)
+        long long sum=w;
+        while(k--)
         {
-            if(k==0)
-                return sum;
-            while(i<arr.size() && w>=arr[i].first)
-            {
-                // cout<<arr[i].first<<endl;
-                st.insert({-arr[i].second,i});
-                i++;
+            while(!q.empty() && sum>=q.top().first){
+                // cout<<q.top().first<<" ";
+                bucket.push(q.top().second);
+                q.pop();
             }
-            if(st.empty())
-                return sum;
-            else
-            {
-                sum=sum+(-st.begin()->first);
-                w=sum;
-                k--;
-                st.erase({st.begin()->first,st.begin()->second});
-            }
-        }
-        // for(auto it: st)
-        // {
-        //     cout<<it.first<<" "<<it.second<<" ";
-        // }
-        while(k-- && !st.empty())
-        {
-                sum=sum-st.begin()->first;
-                st.erase({st.begin()->first,st.begin()->second});
+            ////////////cout<<endl;
+            if(!bucket.empty()){
+                // cout<<bucket.top()<<" ";
+                sum+=bucket.top();
+                bucket.pop();
+            }  
         }
         return sum;
-        
     }
 };
