c82767d1dd13189933934ddb50af86a7cd07f933
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-03-22 20:47:46 +0530

    Time: 471 ms (76.46%), Space: 128.2 MB (63.39%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
         vector<bool>vis(n+1,0);
-        vector<int>graph[n+1];
+        vector<pair<int,int>>graph[n+1];
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         for(int i=0;i<roads.size();i++)
         {
             pq.push({roads[i][2],i});
-            graph[roads[i][0]].push_back(roads[i][1]);
-            graph[roads[i][1]].push_back(roads[i][0]);
+            graph[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+            graph[roads[i][1]].push_back({roads[i][0],roads[i][2]});
         }
+        int ans=INT_MAX;
         queue<int>q;
         q.push(1);
         vis[1]=1;
@@ -20,25 +21,22 @@ public:
             {
                auto a=q.front();
                 q.pop();
+                
                 for(int i=0;i<graph[a].size();i++)
                 {
-                    if(vis[graph[a][i]]==0)
+                    ans=min(ans,graph[a][i].second);
+                    if(vis[graph[a][i].first]==0)
                     {
-                        q.push(graph[a][i]);
-                        vis[graph[a][i]]=1;
+                        
+                        
+                        q.push(graph[a][i].first);
+                        vis[graph[a][i].first]=1;
                     }
                 }
             }
         }
-        while(!pq.empty())
-        {
-            auto a=pq.top();
-            // cout<<a.first<<" ";
-            if(vis[roads[a.second][1]] || vis[roads[a.second][0]])
-                return a.first;
-            pq.pop();
-        }
-        return -1;
+       
+        return ans;;
         
     }
 };
