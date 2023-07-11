8449c5dc342182826704db7b9d821ef686bf3cb0
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-07-12 01:47:56 +0530

    Time: 4 ms (79.96%), Space: 12.4 MB (96.13%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,59 +9,41 @@
  */
 class Solution {
 public:
-    bool search(TreeNode* root,TreeNode* target,vector<TreeNode*>&mp)
-    {
+    int dfs(vector<int> &ans,TreeNode *root,TreeNode* target,bool &flagdown,int &k,int curr){
         if(root==NULL)
-            return 0 ;
-        if(root==target)
-        {
-            return 1;
+            return 0;
+        if(root==target){
+            flagdown=1;
         }
-        bool a=search(root->left,target,mp);
-        bool b=search(root->right,target,mp);
-        if(a==1)
-        {
-            mp.push_back(root);
-            root->left=NULL;
-            return 1;
-        }
-         if(b==1)
-        {
-            mp.push_back(root);
-            root->right=NULL;
-             return 1;
-        }
-        return 0;
-            
-    }
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        queue<pair<TreeNode*,int>>q;
-       vector<TreeNode*> mp;
-        if(target==root)
-        {
-            q.push({root,0});
-        }
+        int l=0,r=0;
+        l=dfs(ans,root->left,target,flagdown,k,curr+flagdown);
+        if(l)
+        r=dfs(ans,root->right,target,flagdown,k,l+flagdown);
         else
-        {
-            search(root,target,mp);
-            q.push({target,0});
-            for(int i=0;i<mp.size();i++)
-            {
-                q.push({mp[i],i+1});
-            }
-        }
+        r=dfs(ans,root->right,target,flagdown,k,curr+flagdown);
+        if(r)
+        l=dfs(ans,root->left,target,flagdown,k,r+flagdown);
+        if(curr==k || max(l,r)==k)
+            ans.push_back(root->val);
+        if(root==target || max(l,r))
+            return 1+max(l,r);
+        return 0;
+        
+       
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         vector<int>ans;
-        while(!q.empty())
-        {
-            auto a=q.front();
-            q.pop();
-            // cout<<a.first->val<<" "<<a.second<<"  ";
-            if(a.second==k)
-                ans.push_back(a.first->val);
-            if(a.first->left)q.push({a.first->left,a.second+1});
-            if(a.first->right)q.push({a.first->right,a.second+1});
-            
+        if(k==0){
+            ans.push_back(target->val);
+            return ans;
         }
-        return ans;           
+        int curr=0;
+        bool flag=0;
+        int flagup=-1;
+        dfs(ans,root,target,flag,k,curr);
+        return ans;
+        
+        
     }
 };
