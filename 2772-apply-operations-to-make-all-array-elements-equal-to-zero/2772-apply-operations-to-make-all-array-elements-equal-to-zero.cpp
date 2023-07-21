6541cdf9b36bd10a79e6e6a44fea7326dbc8a894
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-07-22 00:51:44 +0530

    Time: 189 ms (40.55%), Space: 101.5 MB (11.12%) - LeetHub

---
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool check(vector<int>nums,int k)
+    {
+        if(nums.size()==1 || k==1)
+            return 1;
+        queue<int>q;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums.size()-1==i)
+            {
+                if(sum!=nums[i])
+                    return 0;
+                
+            }
+            if(sum>nums[i])
+                return 0;
+            else
+                nums[i]-=sum;
+            q.push(nums[i]);
+            sum+=nums[i];
+            if(q.size()==k)
+            {
+                sum-=q.front();
+                q.pop();
+            }
+        }
+        return 1;
+    }
+    bool checkArray(vector<int>& nums, int k) {
+       int a=check(nums,k);
+       // reverse(nums.begin(),nums.end());
+       //  int b=check(nums,k);
+        return a;
+        
+        
+        
+    }
+};
