e7c7c67b629ef7a7ade2e1aca378560033b84b92
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-07-25 00:27:32 +0530

    Time: 4 ms (40.18%), Space: 5.9 MB (92.36%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, long n) {
+        if(n==0)
+            return 1;
+        
+
+        if(n<0)
+        {
+            n=(-n);
+            x=1/x;
+        }
+        double ans=1;
+        while(n>0)
+        {
+            if(n&1) ans=ans*x;
+            x=x*x;
+            n=n>>1;
+            
+        }
+        return ans;
+        
+    }
+};
