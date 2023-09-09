ebbc48447bf0280873b086c08703dd5fa0c3c2d0
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-09-09 23:49:45 +0530

    Time: 3 ms (50.37%), Space: 8.4 MB (6.68%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned int>dp(target+1,0); // Approach for best space complexity
-            dp[0]=1;
-        for(int i=1;i<=target;i++)
-        {
-            for(int j=0;j<nums.size();j++)
-            {
-                if(i>=nums[j])
-                {
-                    dp[i]=dp[i]+dp[i-nums[j]];
-                    // cout<<dp[i]<<" ";
-                }
-               
-            }
-             // cout<<endl;
-        }
-        return dp[target];
+        sort(nums.begin(),nums.end());
+        vector<vector<unsigned long long >>dp(target+1,vector<unsigned long long>(nums.size()+1,0));
+        dp[0][0]=1;
+        for(int i=0;i<=target;i++) {
         
+            for(int j=1;j<=nums.size();j++) {
+                dp[i][j]=dp[i][j-1]+(i-nums[j-1]>=0?dp[i-nums[j-1]][nums.size()]:0);
+                // cout<<dp[i][j]<<" ";
+            
+            }
+            // cout<<endl;
+        }
+        return dp[target][nums.size()];
     }
 };
