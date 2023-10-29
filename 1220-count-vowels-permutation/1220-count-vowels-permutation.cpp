76479fb1a7cbc73f6bf11d27aa49c6c830dc2dbe
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-10-29 18:43:21 +0530

    Time: 85 ms (26.56%), Space: 18.6 MB (52.13%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,24 +1,24 @@
-
 class Solution {
 public:
-     vector<vector<int>> relation={{1,2,4},{0,2},{1,3},{2},{2,3}};  //Relationships(can be followed after) of 
-    int countVowelPermutation(int n) 
-    {
-        int MOD=1e9+7;
-        vector<long long> vowels(5,1),vowels_copy;      //When N=1 all vowels are used once to for 1 letter strings
-        long long result=0;
-        while(--n)
-        {
-            vowels_copy=vowels;
-            for(int i=0;i<5;i++)                         //Characters 'a' 'e' 'i' 'o' 'u'.
-            {
-                vowels[i]=0;
-                for(int &r:relation[i])
-                    vowels[i]+=vowels_copy[r],vowels[i]%=MOD;  //Add the strings that end with characters that can have 'i' after them.
+    int countVowelPermutation(int n) {
+        vector<vector<int>>graph={{1},{0,2},{0,1,3,4},{2,4},{0}};
+        int mod=1e9+7;
+        vector<int>dp(5,1);
+        n--;
+        int sum=5;
+        while(n--){
+            sum=0;
+            vector<int>copyDp(5,0);
+            for(int i=0;i<5;i++) {
+                for(int j=0;j<graph[i].size();j++) {
+                    copyDp[graph[i][j]]=((copyDp[graph[i][j]]%mod)+(dp[i]%mod))%mod;
+                    sum=((sum%mod)+(dp[i]%mod))%mod;
+                }
+                // cout<<sum<<" ";
             }
+            dp=copyDp;
         }
-        for(long long &i:vowels)
-            result+=i,result%=MOD;
-        return result;
+        return sum;
+        
     }
 };
