3b686849eabf1cbc8933a6ee2a4e1472073cf648
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-11-30 01:08:43 +0530

    Time: 37 ms (55.89%), Space: 22.5 MB (8.97%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int recur(char a,string ans, vector<int>characters,int sum){
+        ans.push_back(a);
+        sum--;
+        // cout<<a-'A'<<" ";
+        characters[a-'A']--;
+        // cout<<ans<<" ";
+        int x=1;
+        if(sum==0){
+            return x;
+        }
+        for(int i=0;i<characters.size();i++){
+            if(characters[i]!=0){
+                x=x+recur(i+'A',ans,characters,sum);
+            }
+        }
+        return x;
+    }
+    int numTilePossibilities(string tiles) {
+        vector<int>characters(26,0);
+        int sum=0;
+        for(int i=0;i<tiles.size();i++) {
+            characters[tiles[i]-'A']++;
+        }
+        sum=tiles.size();
+        string ans;
+        int x=0;
+       
+        for(int i=0;i<characters.size();i++){
+            if(characters[i]!=0){
+                x=x+recur(i+'A',ans,characters,sum);
+            }
+        }
+        return x;
+        
+    }
+};
