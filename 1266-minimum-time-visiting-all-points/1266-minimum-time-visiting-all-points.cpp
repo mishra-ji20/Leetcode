b1e3c31a6633aa1bb3f3fa28e37abe3b200f7fdf
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2023-12-03 12:18:35 +0530

    Time: 7 ms (60.44%), Space: 10.6 MB (14.02%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -3,9 +3,7 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int sum=0;
         for(int i=1;i<points.size();i++){
-            int xDist=abs(points[i][0]-points[i-1][0]);
-            int yDist=abs(points[i][1]-points[i-1][1]);
-            sum+=min(xDist,yDist)+abs(xDist-yDist);
+            sum+=min(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]))+abs(abs(points[i][0]-points[i-1][0])-abs(points[i][1]-points[i-1][1]));
         }
         return sum;
         
