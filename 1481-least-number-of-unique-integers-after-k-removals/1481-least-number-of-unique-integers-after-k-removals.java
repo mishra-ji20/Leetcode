35ae2caf389eea2b6d650d2d89e3082cf7a8e033
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2024-02-16 23:33:50 +0530

    Time: 47 ms (73.49%), Space: 64 MB (9.59%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -4,10 +4,8 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
         }
-        ArrayList<Integer>nums=new ArrayList<>();
-        for(Map.Entry<Integer,Integer> elem: hm.entrySet()){
-            nums.add(elem.getValue());
-        }
+        ArrayList<Integer>nums=new ArrayList<>(hm.values());
+
         Collections.sort(nums);
         for(int i=0;i<nums.size();i++) {
             if(nums.get(i)<=k){
