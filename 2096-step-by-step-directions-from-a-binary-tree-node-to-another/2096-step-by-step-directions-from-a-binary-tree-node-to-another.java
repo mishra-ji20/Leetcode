0601cafade936b1e8bd747e6a52598ed23c05763
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2024-08-16 00:24:27 +0530

    Time: 33 ms (42.09%), Space: 79.8 MB (62.51%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -14,30 +14,7 @@
  * }
  */
 class Solution {
-    TreeNode lowestCommonAncestor ;
-    StringBuilder start;
-    StringBuilder dest;
-    public boolean getStartPath(TreeNode root, int dest ,StringBuilder path){
-        if(root == null){
-            return false;
-        }
-        if(root.val == dest){
-            this.start = path;
-            return true;
-        }
-        path.append("U");
-        if(getStartPath(root.left,dest,path)){
-            return true;
-        }
-        
-        if(getStartPath(root.right,dest,path)){
-            return true;
-        }
-        path.setLength(path.length() - 1);
-        return false;
-        
-    }
-    public boolean getDestPath(TreeNode root,int dest ,StringBuilder path){
+    public boolean getPath(TreeNode root,int dest ,StringBuilder path){
         // System.out.println(path);
         
         if(root == null){
@@ -46,17 +23,16 @@ class Solution {
         }
         if(root.val == dest){
             // System.out.println("1");
-            this.dest = path;
             return true;
         }
         path.append("L");
-       if( getDestPath(root.left,dest,path)){
+       if( getPath(root.left,dest,path)){
            return true;
        }
         path.setLength(path.length()-1);
         path.append("R");
        
-        if( getDestPath(root.right,dest,path)){
+        if( getPath(root.right,dest,path)){
             return true;
         }
         path.setLength(path.length()-1);
@@ -64,36 +40,24 @@ class Solution {
         
         
     }
-    public  boolean getlowestCommonAncestor(TreeNode root ,int startValue, int destValue){
-        if(root == null){
-            return false;
-        }
-        boolean rightFlag = getlowestCommonAncestor(root.right,startValue ,destValue);
-        boolean leftFlag  = getlowestCommonAncestor(root.left,startValue ,destValue);
-        
-        boolean selfFlag = (root.val == startValue || root.val == destValue) ;
-        
-        if((rightFlag & leftFlag) || (rightFlag & selfFlag) || (leftFlag & selfFlag)  ){
-            lowestCommonAncestor =root;
-        }
-         return (rightFlag | leftFlag | selfFlag); 
-        
-    }
     public String getDirections(TreeNode root, int startValue, int destValue) {
-        getlowestCommonAncestor(root,startValue,destValue);
         
-        var x= lowestCommonAncestor;
         // System.out.println(x.val);
+        StringBuilder start = new StringBuilder();
+        StringBuilder dest = new StringBuilder();
+        StringBuilder path =new StringBuilder();
+        getPath(root,startValue,start);                
+        getPath(root,destValue,dest);
+        int index =0;
+        for(index=0 ;index<start.length() && index<dest.length();index++){
+            if(start.charAt(index)!=dest.charAt(index)){
+                break;
+            }
+        }
+        path.append("U".repeat(start.length()-(index)));
+        path.append(dest.substring(index));
+        return path.toString();
         
-        getStartPath(lowestCommonAncestor,startValue,new StringBuilder());
-        
-        // System.out.println(start) ;
-        // System.out.println(dest);
-        
-        
-        getDestPath(lowestCommonAncestor,destValue,new StringBuilder());
-        start.append(dest);
-        return start.toString();
         
     }
 }
