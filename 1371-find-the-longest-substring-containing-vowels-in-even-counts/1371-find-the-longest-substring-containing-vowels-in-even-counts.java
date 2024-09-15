40670688f09e18c0f8f562f7efd87da101fa4767
mishra-ji20 <91391362+mishra-ji20@users.noreply.github.com>
2024-09-16 00:52:33 +0530

    Time: 68 ms (25.98%), Space: 48.6 MB (35.59%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -1,35 +1,24 @@
 class Solution {
-    boolean checkVowels(char ch){
-        if(ch== 'a' || ch == 'e' || ch == 'i' || ch =='o' || ch == 'u' )
-            return true;
-        return false;
-        
-    }
-    int createHash(int [] arr){
-        int hash =0;
-        for(int i=0;i<arr.length;i++){
-            hash = ((hash )<<1) | arr[i];
-        }
-        return hash;
-    }
     public int findTheLongestSubstring(String s) {
+        HashMap<Character,Integer> characterValue =new  HashMap<Character,Integer> ();
+        characterValue.put('a',2);
+        characterValue.put('e',4);
+        characterValue.put('i',8);
+        characterValue.put('o',16);
+        characterValue.put('u',32);
+        
+        
         HashMap <Integer,Integer> lastOccurenceIndex = new HashMap<Integer,Integer>();
         lastOccurenceIndex.put(0,-1);
         int xorValue = 0;
         int ans = 0;
-        int [] vowelsCount=  new int [26];
         for(int i=0 ;i<s.length();i++){
-            if(checkVowels(s.charAt(i))){
-                vowelsCount[s.charAt(i)-'a'] =  vowelsCount[s.charAt(i)-'a'] ^ 1;
+            xorValue  = xorValue ^ (characterValue.getOrDefault(s.charAt(i),0));
+            if(lastOccurenceIndex.getOrDefault(xorValue,-2)==-2){
+               lastOccurenceIndex.put(xorValue,i); 
             }
-            var hash = createHash(vowelsCount); 
-            if(lastOccurenceIndex.getOrDefault(hash,-2) == -2){
-                // System.out.print(lastOccurenceIndex.getOrDefault(hash,-2)+" ");
-                lastOccurenceIndex.put(hash,i);
-            } 
-            ans =Math.max(ans,i-lastOccurenceIndex.getOrDefault(hash,i)); 
-            // System.out.println(hash+" "+ lastOccurenceIndex.getOrDefault(hash,-2) +"      ");
-          
+            ans = Math.max(ans,i - lastOccurenceIndex.getOrDefault(xorValue,i));
+            // System.out.println(xorValue+" "+ans + "       ");
         }
         // System.out.println("");
         return ans;
